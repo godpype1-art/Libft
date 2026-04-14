42b37a0c6c42c53733c6a3fa077d1a76a14e8983
Filipe Alves Esteves De Almeida Cabrito <falves-e@c1r9s9.42lisboa.com>
2026-04-14 20:14:20 +0100

    a

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: falves-e <falves-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 15:58:30 by falves-e          #+#    #+#             */
-/*   Updated: 2026/04/14 19:33:24 by falves-e         ###   ########.fr       */
+/*   Updated: 2026/04/14 19:35:10 by falves-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
