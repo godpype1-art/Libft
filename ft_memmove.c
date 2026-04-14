61de9c108fd7190a34a40d4ac0c472f053e6db89
Filipe Alves Esteves De Almeida Cabrito <falves-e@c1r4s5.42lisboa.com>
2026-04-14 18:03:47 +0100

    a

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: falves-e <falves-e@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 17:19:46 by falves-e          #+#    #+#             */
-/*   Updated: 2026/04/14 17:31:31 by falves-e         ###   ########.fr       */
+/*   Updated: 2026/04/14 18:02:32 by falves-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,28 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t				i;
-	unsigned char		*temp;
 	const unsigned char	*origin;
 	unsigned char		*final;
 
 	i = 0;
 	origin = (const unsigned char *) src;
 	final = (unsigned char *) dest;
-	while (i < n)
+	if (final <= origin)
 	{
-		temp[i] = origin[i];
-		i++;
+			while (i < n)
+			{
+				final[i] = origin[i];
+				i++;
+			}
 	}
-	i = 0;
-	while (i < n)
+	else
 	{
-		final[i] = temp[i];
-		i++;
+		i = n;
+			while (i > 0)
+			{
+				i--;
+				final[i] = origin[i];
+			}
 	}
 	return (dest);
 }
