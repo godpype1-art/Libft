dc0172033285add013a1d63e88509075b49be960
Filipe <godpype1@gmail.com>
2026-04-22 17:54:14 +0100

    a

--- ft_split.c
+++ ft_split.c
@@ -16,15 +16,28 @@ size_t	count_words(char const *s, char c)
 {
 	size_t	i;
 	size_t	word_count;
+	int		in_word;
 
 	word_count = 0;
 	i = 0;
+	in_word == 0;
 	while (s[i] != '\0')
 	{
-		
+		if (s[i] != c && in_word == 0)
+		{
+			in_word = 1;
+			word_count++;
+		}
+		else if (s[i] == c)
+			in_word = 0;
 		i++;
 	}
 	return (word_count);
+}
+
+char	*fill_word(char const *s, char c)
+{
+
 }
 
 char	**ft_split(char const *s, char c)
@@ -35,17 +48,22 @@ char	**ft_split(char const *s, char c)
 
 	i = 0;
 	word_count = count_word(s, c);
-	array = malloc(sizeof(char *) * word_count + 1);
+	array = malloc(sizeof(char *) * (word_count + 1));
+	if (array == NULL)
+		return (NULL);
 	while (i < word_count)
 	{
-		*array[i] = fill_word(s);
+		while (*s == c)
+			s++;
+		array[i] = fill_word(s, c);
 		if (array[i] == NULL)
 		{
 			free_all(array);
 			return (NULL);
 		}
+		s = ft_strchr(s,c);
 		i++;
 	}
-	*array[i] = NULL;
+	array[i] = NULL;
 	return (array);
 }
