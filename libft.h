00b5fff9456611477d45d7a5b3801bb6d85f8738
Filipe <godpype1@gmail.com>
2026-04-22 18:46:30 +0100

    a

--- libft.h
+++ libft.h
@@ -44,6 +44,6 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-int main(void);
+int		main(void);
 
 #endif
--- srcs/ft_isalpha.c
+++ srcs/ft_isalpha.c
@@ -18,4 +18,3 @@ int	ft_isalpha(char c)
 		return (1);
 	return (0);
 }
-
